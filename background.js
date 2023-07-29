8b9dad56431fc7b445d608f37e3473a892883b2a
Decrypt <louis.vicomte@protonmail.com>
2023-07-29 13:18:34 +0200

    fix

--- background.js
+++ background.js
@@ -1,11 +1,30 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {cancel: true};
+[
+    {
+      "id": 1,
+      "priority": 1,
+      "action": { "type": "block" },
+      "condition": {
+        "urlFilter": "*://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js",
+        "resourceTypes": ["script"]
+      }
     },
-    {urls: [
-        "*://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js",
-        "*://cdn.onesignal.com/sdks/OneSignalSDK.js",
-        "*://cryptoast.fr/wp-content/plugins/page-links-to/dist/new-tab.js"
-    ]},
-    ["blocking"]
-);
+    {
+      "id": 2,
+      "priority": 1,
+      "action": { "type": "block" },
+      "condition": {
+        "urlFilter": "*://cdn.onesignal.com/sdks/OneSignalSDK.js",
+        "resourceTypes": ["script"]
+      }
+    },
+    {
+      "id": 3,
+      "priority": 1,
+      "action": { "type": "block" },
+      "condition": {
+        "urlFilter": "*://cryptoast.fr/wp-content/plugins/page-links-to/dist/new-tab.js",
+        "resourceTypes": ["script"]
+      }
+    }
+  ]
+  
