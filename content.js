05edfe03e0575c61e16d56dfbff5eaec5f4357e8
Decrypt <louis.vicomte@protonmail.com>
2023-07-30 17:06:04 +0200

    null prevent

--- content.js
+++ content.js
@@ -75,15 +75,19 @@ document.querySelectorAll('img').forEach(function(img) {
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  var articleHeight = document.querySelector('.article-main-ctn').scrollHeight;
-  var scrollPosition = window.pageYOffset;
-  var windowHeight = window.innerHeight;
-  var scrollHeight = articleHeight - windowHeight;
-  var progress = (scrollPosition / scrollHeight) * 100;
+  var articleElement = document.querySelector('.article-main-ctn');
+  if (articleElement) {
+    var articleHeight = articleElement.scrollHeight;
+    var scrollPosition = window.pageYOffset;
+    var windowHeight = window.innerHeight;
+    var scrollHeight = articleHeight - windowHeight;
+    var progress = (scrollPosition / scrollHeight) * 100;
 
-  document.getElementById('myBar').style.width = progress + '%';
+    document.getElementById('myBar').style.width = progress + '%';
+  }
 }
 
 var bar = document.createElement('div');
 bar.setAttribute('id', 'myBar');
 document.body.appendChild(bar);
+
