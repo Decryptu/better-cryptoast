7815aa878e8587bc2df2e3eff00f6a5cbad06254
Decrypt <louis.vicomte@protonmail.com>
2023-07-30 16:56:33 +0200

    progress bar reader, bug fixes, new readme

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-Welcome to Better Cryptoast, your go-to Chrome extension for a superior experience on Cryptoast.fr. Our extension is designed to streamline your interaction with the Cryptoast website, enhancing it in several key ways for an optimal user experience.
+Name: Better Cryptoast
 
-Here's what Better Cryptoast offers:
+Description: 
 
-CSS Enhancements: We have completely revamped the aesthetic experience on the site with a sleek, modern design. Our sophisticated CSS improvements make the site more visually appealing and easier to navigate.
+Enhance your browsing experience on Cryptoast.fr with the Better Cryptoast extension.
 
-Bug Fixes: We believe that a seamless user experience is paramount. We've tirelessly worked to identify and rectify any potential bugs, making your navigation on the site smooth and trouble-free.
+1. **Live Bitcoin Price:** Stay updated with the latest Bitcoin price. This extension fetches real-time Bitcoin price in USD from Coingecko and displays it in the header, allowing you to keep an eye on Bitcoin's value without the need to navigate away from your current page.
 
-Exciting Features: We've incorporated a plethora of cool features, like auto-detection for light and dark themes, a user-friendly toggle switch for your extension, stylish hover effects, and much more.
+2. **Image Replacer & Enhancer:** Optimize your visual experience on the site. The extension replaces specific images on the page with more optimized versions, offering uncompressed, better-quality images.
 
-In essence, Better Cryptoast is designed to make your Cryptoast.fr browsing experience enjoyable, efficient, and hassle-free. Install the extension today and see the difference!
+3. **Reading Progress Indicator:** Enjoy a streamlined reading experience with our easy-to-use progress bar. As you scroll through articles (containing the class `article-main-ctn`), a progress bar at the top of the page fills up, indicating how much of the page you've scrolled through.
+
+4. **In-Built Search Feature:** The extension's popup features a built-in search field that allows you to search for content directly on Cryptoast. Find what you need without leaving your current page, improving efficiency and saving time.
+
+5. **Bug Fixes:** Better Cryptoast offers a more consistent and reliable user experience by fixing various bugs. This includes issues like stretched-out thumbnails, the website not working in windowed mode, and instances of text not being visible. The extension smoothens your browsing experience by tackling these issues head-on.
+
+6. **Block Slow Scripts:** For a faster and more private browsing experience, the extension blocks slow and unnecessary scripts, including redundant JavaScript and scripts used for cookie tracking.
+
+7. **Automatic Theme Detection & Enhanced Styles:** Better Cryptoast automatically detects your preferred theme (dark or light) and adjusts the website's interface accordingly. It also introduces additional CSS for an enhanced, visually pleasing user experience.
+
+Upgrade your Cryptoast.fr browsing experience today with the Better Cryptoast extension - making your reading more efficient, engaging, and free from distractions.
--- content.js
+++ content.js
@@ -1,6 +1,89 @@
 window.onload = function() {
     let element = document.querySelector('.header-title.ms-2.me-1.bold');
     if (element) {
-        element.textContent += '✨';
+        // Create a span and add the sparkle emoji to it
+        let span = document.createElement('span');
+        span.textContent = '✨';
+        span.classList.add('sparkle');
+
+        // Append the span to the target element
+        element.appendChild(span);
+
+        // Create a new style element
+        let style = document.createElement('style');
+
+        // Define CSS as text
+        style.textContent = `
+        @keyframes flicker {
+            0% {opacity: 1;}
+            50% {opacity: 0.5;}
+            100% {opacity: 1;}
+        }
+        .sparkle {
+            animation: flicker 2s linear infinite;
+        }
+        `;
+
+        // Append the style to the document head
+        document.head.appendChild(style);
     }
 }
+
+// Fetch BTC price and display in the header
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const bitcoinPrice = Math.round(data.bitcoin.usd);
+        const formattedBitcoinPrice = bitcoinPrice.toLocaleString('fr-FR');
+        const anchorElement = document.querySelector('.buy-crypto.d-flex.flex-row.align-items-center.cursor-pointer');
+        if(anchorElement && anchorElement.querySelector){
+            const spanElement = anchorElement.querySelector('.text-nowrap.fw-bold');
+            if (spanElement) {
+                spanElement.textContent = `${formattedBitcoinPrice}$`;
+            }
+        }
+    })
+    .catch(err => console.error(err));
+
+// Fetch all the images on the page.
+const images = document.getElementsByTagName('img');
+
+// Iterate over each image.
+for (let i = 0; i < images.length; i++) {
+    // Check if the image src matches the one you want to replace.
+    if (images[i].src === 'https://cryptoast.fr/wp-content/uploads/2023/01/Sans-titre-1-300x300.png') {
+        // Replace the image src with the new URL.
+        images[i].src = 'https://i.imgur.com/X4bya8i.png';
+    }
+}
+
+// Iterate over all image elements on the page
+document.querySelectorAll('img').forEach(function(img) {
+    // Check if the image src contains '-300x150'
+    if (img.src && img.src.includes('-300x150')) {
+        // Replace '-300x150' with an empty string to get the uncompressed image
+        img.src = img.src.replace('-300x150', '');
+    }
+});
+
+// Add a progress bar on the top of the articles
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var articleHeight = document.querySelector('.article-main-ctn').scrollHeight;
+  var scrollPosition = window.pageYOffset;
+  var windowHeight = window.innerHeight;
+  var scrollHeight = articleHeight - windowHeight;
+  var progress = (scrollPosition / scrollHeight) * 100;
+
+  document.getElementById('myBar').style.width = progress + '%';
+}
+
+var bar = document.createElement('div');
+bar.setAttribute('id', 'myBar');
+document.body.appendChild(bar);
