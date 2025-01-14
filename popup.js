f101dbeb9a04fb9152356801002271975b4d7c12
Decrypt <louis.vicomte@protonmail.com>
2025-01-14 11:59:34 +0100

    google search

--- popup.js
+++ popup.js
@@ -1,151 +1,168 @@
-document.getElementById('searchForm').addEventListener('submit', (event) => {
-    event.preventDefault();
-  
-    const searchQuery = document.getElementById('searchInput').value;
-    const url = `https://cryptoast.fr/?s=${encodeURIComponent(searchQuery)}`;
-  
-    // Open the URL in a new tab
-    chrome.tabs.create({ url: url });
-  });
-  
-  const countdownContainer = document.querySelector("#countdown");
+document.getElementById("searchForm").addEventListener("submit", (event) => {
+	event.preventDefault();
 
-  const TARGET_BLOCK_HEIGHT = 1050000;
-  const AVERAGE_BLOCK_TIME = 600; // In seconds (10 minutes)
-  
-  // This will be the time in seconds till the halving event.
-  let timeTillHalving = 0;
-  
-  function fetchCurrentBlockHeight() {
-      return fetch('https://blockchain.info/q/getblockcount')
-          .then(response => response.text())
-          .then(currentBlockHeight => {
-              const blocksLeft = TARGET_BLOCK_HEIGHT - currentBlockHeight;
-              timeTillHalving = blocksLeft * AVERAGE_BLOCK_TIME;
-          });
-  }
-  
-  function updateCountdown() {
-      timeTillHalving = Math.max(timeTillHalving - 1, 0);
-    
-      const days = Math.floor(timeTillHalving / 60 / 60 / 24);
-      const hours = Math.floor(timeTillHalving / 60 / 60 % 24);
-      const minutes = Math.floor(timeTillHalving / 60 % 60);
-      const seconds = Math.floor(timeTillHalving % 60);
-    
-      countdownContainer.innerHTML = `
+	const searchQuery = document.getElementById("searchInput").value;
+	const url = `https://www.google.com/search?q=site:cryptoast.fr+${encodeURIComponent(searchQuery)}`;
+
+	// Open the URL in a new tab
+	chrome.tabs.create({ url: url });
+});
+
+const countdownContainer = document.querySelector("#countdown");
+
+const TARGET_BLOCK_HEIGHT = 1050000;
+const AVERAGE_BLOCK_TIME = 600; // In seconds (10 minutes)
+
+// This will be the time in seconds till the halving event.
+let timeTillHalving = 0;
+
+function fetchCurrentBlockHeight() {
+	return fetch("https://blockchain.info/q/getblockcount")
+		.then((response) => response.text())
+		.then((currentBlockHeight) => {
+			const blocksLeft = TARGET_BLOCK_HEIGHT - currentBlockHeight;
+			timeTillHalving = blocksLeft * AVERAGE_BLOCK_TIME;
+		});
+}
+
+function updateCountdown() {
+	timeTillHalving = Math.max(timeTillHalving - 1, 0);
+
+	const days = Math.floor(timeTillHalving / 60 / 60 / 24);
+	const hours = Math.floor((timeTillHalving / 60 / 60) % 24);
+	const minutes = Math.floor((timeTillHalving / 60) % 60);
+	const seconds = Math.floor(timeTillHalving % 60);
+
+	countdownContainer.innerHTML = `
         <div class="time-segment">
-          <span class="number">${days.toString().padStart(2, '0')}</span>
-          <span class="time-unit">jour${days !== 1 ? 's' : ''}</span>
+          <span class="number">${days.toString().padStart(2, "0")}</span>
+          <span class="time-unit">jour${days !== 1 ? "s" : ""}</span>
         </div>
         :
         <div class="time-segment">
-          <span class="number">${hours.toString().padStart(2, '0')}</span>
-          <span class="time-unit">heure${hours !== 1 ? 's' : ''}</span>
+          <span class="number">${hours.toString().padStart(2, "0")}</span>
+          <span class="time-unit">heure${hours !== 1 ? "s" : ""}</span>
         </div>
         :
         <div class="time-segment">
-          <span class="number">${minutes.toString().padStart(2, '0')}</span>
-          <span class="time-unit">minute${minutes !== 1 ? 's' : ''}</span>
+          <span class="number">${minutes.toString().padStart(2, "0")}</span>
+          <span class="time-unit">minute${minutes !== 1 ? "s" : ""}</span>
         </div>
         :
         <div class="time-segment">
-          <span class="number">${seconds.toString().padStart(2, '0')}</span>
-          <span class="time-unit">seconde${seconds !== 1 ? 's' : ''}</span>
+          <span class="number">${seconds.toString().padStart(2, "0")}</span>
+          <span class="time-unit">seconde${seconds !== 1 ? "s" : ""}</span>
         </div>
       `;
-  }
-  
-  const etaContainer = document.querySelector("#eta-time");
-  
-  function updateETA() {
-      const etaDate = new Date(Date.now() + timeTillHalving * 1000); // Convert seconds to milliseconds
-  
-      const etaDay = etaDate.getUTCDate().toString().padStart(2, '0');
-      const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
-      const etaMonth = months[etaDate.getUTCMonth()];
-      const etaYear = etaDate.getUTCFullYear();
-      const etaHours = etaDate.getUTCHours().toString().padStart(2, '0');
-      const etaMinutes = etaDate.getUTCMinutes().toString().padStart(2, '0');
-  
-      etaContainer.textContent = `${etaDay} ${etaMonth} ${etaYear}, ${etaHours}:${etaMinutes} UTC`;
-  }
-  
-  function updateCountdownAndETA() {
-      updateCountdown();
-      updateETA();
-  }
-  
-  fetchCurrentBlockHeight().then(() => {
-      updateCountdownAndETA(); // Initial update
-      setInterval(updateCountdownAndETA, 1000);
-  });
-  
+}
+
+const etaContainer = document.querySelector("#eta-time");
+
+function updateETA() {
+	const etaDate = new Date(Date.now() + timeTillHalving * 1000); // Convert seconds to milliseconds
+
+	const etaDay = etaDate.getUTCDate().toString().padStart(2, "0");
+	const months = [
+		"janvier",
+		"février",
+		"mars",
+		"avril",
+		"mai",
+		"juin",
+		"juillet",
+		"août",
+		"septembre",
+		"octobre",
+		"novembre",
+		"décembre",
+	];
+	const etaMonth = months[etaDate.getUTCMonth()];
+	const etaYear = etaDate.getUTCFullYear();
+	const etaHours = etaDate.getUTCHours().toString().padStart(2, "0");
+	const etaMinutes = etaDate.getUTCMinutes().toString().padStart(2, "0");
+
+	etaContainer.textContent = `${etaDay} ${etaMonth} ${etaYear}, ${etaHours}:${etaMinutes} UTC`;
+}
+
+function updateCountdownAndETA() {
+	updateCountdown();
+	updateETA();
+}
+
+fetchCurrentBlockHeight().then(() => {
+	updateCountdownAndETA(); // Initial update
+	setInterval(updateCountdownAndETA, 1000);
+});
+
 // Price converter
 let btcToUsdRate = 0;
 
-document.getElementById('btcInput').addEventListener('input', updateUSDValue);
-document.getElementById('usdInput').addEventListener('input', updateBTCValue);
+document.getElementById("btcInput").addEventListener("input", updateUSDValue);
+document.getElementById("usdInput").addEventListener("input", updateBTCValue);
 
 // Get the current Bitcoin price from CoinGecko
 function fetchBTCPrice() {
-    fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-        .then(response => response.json())
-        .then(data => {
-            btcToUsdRate = data.bitcoin.usd;
-        })
-        .catch(error => {
-            console.error("There was an error fetching the BTC price:", error);
-        });
+	fetch(
+		"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			btcToUsdRate = data.bitcoin.usd;
+		})
+		.catch((error) => {
+			console.error("There was an error fetching the BTC price:", error);
+		});
 }
 
 // Update the USD input based on the BTC input
 function updateUSDValue() {
-    const btcValue = Number.parseFloat(document.getElementById('btcInput').value);
-    if (!Number.isNaN(btcValue)) {
-        const usdValue = btcToUsdRate * btcValue;
-        document.getElementById('usdInput').value = usdValue.toFixed(2);
-    }
+	const btcValue = Number.parseFloat(document.getElementById("btcInput").value);
+	if (!Number.isNaN(btcValue)) {
+		const usdValue = btcToUsdRate * btcValue;
+		document.getElementById("usdInput").value = usdValue.toFixed(2);
+	}
 }
 
 // Update the BTC input based on the USD input
 function updateBTCValue() {
-    const usdValue = Number.parseFloat(document.getElementById('usdInput').value);
-    if (!Number.isNaN(usdValue)) {
-        const btcValue = usdValue / btcToUsdRate;
-        document.getElementById('btcInput').value = btcValue.toFixed(8);  // BTC can have up to 8 decimals
-    }
+	const usdValue = Number.parseFloat(document.getElementById("usdInput").value);
+	if (!Number.isNaN(usdValue)) {
+		const btcValue = usdValue / btcToUsdRate;
+		document.getElementById("btcInput").value = btcValue.toFixed(8); // BTC can have up to 8 decimals
+	}
 }
 
 // Function to filter input
 function filterInput(event) {
-    // Allow only digits and dots
-    event.target.value = event.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1'); 
-    // The second replace() ensures there's only one dot
+	// Allow only digits and dots
+	event.target.value = event.target.value
+		.replace(/[^0-9.]/g, "")
+		.replace(/(\..*)\./g, "$1");
+	// The second replace() ensures there's only one dot
 }
 
-document.getElementById('btcInput').addEventListener('input', filterInput);
-document.getElementById('usdInput').addEventListener('input', filterInput);
+document.getElementById("btcInput").addEventListener("input", filterInput);
+document.getElementById("usdInput").addEventListener("input", filterInput);
 
 // Fetch the BTC price on popup open
 fetchBTCPrice();
 
 // Add this to the end of popup.js
 
-document.getElementById('saveApiKey').addEventListener('click', () => {
-    const apiKey = document.getElementById('apiKeyInput').value;
-    chrome.runtime.sendMessage({ action: 'setApiKey', apiKey }, (response) => {
-      if (response.success) {
-        alert('API key saved successfully!');
-      } else {
-        alert('Failed to save API key. Please try again.');
-      }
-    });
-  });
-  
-  // Load saved API key when popup opens
-  chrome.runtime.sendMessage({ action: 'getApiKey' }, (response) => {
-    if (response.apiKey) {
-      document.getElementById('apiKeyInput').value = response.apiKey;
-    }
-  });
+document.getElementById("saveApiKey").addEventListener("click", () => {
+	const apiKey = document.getElementById("apiKeyInput").value;
+	chrome.runtime.sendMessage({ action: "setApiKey", apiKey }, (response) => {
+		if (response.success) {
+			alert("API key saved successfully!");
+		} else {
+			alert("Failed to save API key. Please try again.");
+		}
+	});
+});
+
+// Load saved API key when popup opens
+chrome.runtime.sendMessage({ action: "getApiKey" }, (response) => {
+	if (response.apiKey) {
+		document.getElementById("apiKeyInput").value = response.apiKey;
+	}
+});
--- searchBox.js
+++ searchBox.js
@@ -1,82 +1,89 @@
 // Create search box
-let searchContainer = document.createElement('div');
-searchContainer.classList.add('search-container');
-let searchBox = document.createElement('input');
-searchBox.type = 'text';
-searchBox.placeholder = 'Search...';
-searchBox.id = 'searchBox';
-searchBox.classList.add('search-input-ctn');
+const searchContainer = document.createElement("div");
+searchContainer.classList.add("search-container");
+const searchBox = document.createElement("input");
+searchBox.type = "text";
+searchBox.placeholder = "Search...";
+searchBox.id = "searchBox";
+searchBox.classList.add("search-input-ctn");
 searchContainer.appendChild(searchBox);
 
 // Insert search box to the right of the title
-let title = document.querySelector('.news-section-second-title.color-orange-gradient.fw-bold');
+const title = document.querySelector(
+	".news-section-second-title.color-orange-gradient.fw-bold",
+);
 
 if (title) {
-    title.parentNode.insertBefore(searchContainer, title.nextSibling);
+	title.parentNode.insertBefore(searchContainer, title.nextSibling);
 
-    searchBox.addEventListener('keyup', function(e) {
-        let searchString = e.target.value.toLowerCase().replace(/\s+/g, ' ').trim();
-        let searchWords = searchString.split(' ');
+	searchBox.addEventListener("keyup", (e) => {
+		const searchString = e.target.value
+			.toLowerCase()
+			.replace(/\s+/g, " ")
+			.trim();
+		const searchWords = searchString.split(" ");
 
-        let articles = document.querySelectorAll('.last-news-card.card-shadow');
+		const articles = document.querySelectorAll(".last-news-card.card-shadow");
 
-        if (searchString === '') {
-            // Show all articles if the search field is empty
-            articles.forEach(article => {
-                article.style.display = '';
-            });
-        } else {
-            // Apply search filtering when there's a search term
-            articles.forEach(article => {
-                let title = article.querySelector('.last-news-card-title.ps-1.animated-underline span').textContent.toLowerCase();
-                let titleWords = title.split(' ');
+		if (searchString === "") {
+			// Show all articles if the search field is empty
+			for (const article of articles) {
+				article.style.display = "";
+			}
+		} else {
+			// Apply search filtering when there's a search term
+			for (const article of articles) {
+				const title = article
+					.querySelector(".last-news-card-title.ps-1.animated-underline span")
+					.textContent.toLowerCase();
+				const titleWords = title.split(" ");
 
-                let articleShouldBeShown = searchWords.some(searchWord => 
-                    titleWords.some(titleWord => isSimilar(searchWord, titleWord))
-                );
+				const articleShouldBeShown = searchWords.some((searchWord) =>
+					titleWords.some((titleWord) => isSimilar(searchWord, titleWord)),
+				);
 
-                article.style.display = articleShouldBeShown ? '' : 'none';
-            });
-        }
-    });
+				article.style.display = articleShouldBeShown ? "" : "none";
+			}
+		}
+	});
 }
 
 function isSimilar(word1, word2) {
-    // Apply fault tolerance only for words of a certain length to reduce false positives
-    if (word1.length < 4 || word2.length < 4) {
-        return word1 === word2; // No fault tolerance for short words
-    }
-    
-    // Check direct inclusion for longer words
-    if (word1.includes(word2) || word2.includes(word1)) return true;
-    
-    // Apply Levenshtein distance for closer inspection with stricter rules for longer words
-    let distance = levenshteinDistance(word1, word2);
-    if (word1.length >= 5 && word2.length >= 5) {
-        // Allow a smaller ratio of errors for longer words
-        return distance <= Math.floor(Math.max(word1.length, word2.length) / 5);
-    }
-    return distance <= 1; // One character difference for words shorter than 5 characters
+	// Apply fault tolerance only for words of a certain length to reduce false positives
+	if (word1.length < 4 || word2.length < 4) {
+		return word1 === word2; // No fault tolerance for short words
+	}
+
+	// Check direct inclusion for longer words
+	if (word1.includes(word2) || word2.includes(word1)) return true;
+
+	// Apply Levenshtein distance for closer inspection with stricter rules for longer words
+	const distance = levenshteinDistance(word1, word2);
+	if (word1.length >= 5 && word2.length >= 5) {
+		// Allow a smaller ratio of errors for longer words
+		return distance <= Math.floor(Math.max(word1.length, word2.length) / 5);
+	}
+	return distance <= 1; // One character difference for words shorter than 5 characters
 }
 
 // Function to calculate the Levenshtein distance between two strings
 function levenshteinDistance(a, b) {
-    const an = a.length;
-    const bn = b.length;
-    const matrix = Array.from({ length: an + 1 }, () => Array(bn + 1).fill(0));
-    for (let i = 0; i <= an; i++) matrix[i][0] = i;
-    for (let j = 0; j <= bn; j++) matrix[0][j] = j;
+	const an = a.length;
+	const bn = b.length;
+	const matrix = Array.from({ length: an + 1 }, () => Array(bn + 1).fill(0));
+	for (let i = 0; i <= an; i++) matrix[i][0] = i;
+	for (let j = 0; j <= bn; j++) matrix[0][j] = j;
 
-    for (let i = 1; i <= an; i++) {
-        for (let j = 1; j <= bn; j++) {
-            const substitutionCost = a[i - 1] === b[j - 1] ? 0 : 1;
-            matrix[i][j] = Math.min(
-                matrix[i - 1][j] + 1, // deletion
-                matrix[i][j - 1] + 1, // insertion
-                matrix[i - 1][j - 1] + substitutionCost // substitution
-            );
-        }
-    }
+	for (let i = 1; i <= an; i++) {
+		for (let j = 1; j <= bn; j++) {
+			const substitutionCost = a[i - 1] === b[j - 1] ? 0 : 1;
+			matrix[i][j] = Math.min(
+				matrix[i - 1][j] + 1, // deletion
+				matrix[i][j - 1] + 1, // insertion
+				matrix[i - 1][j - 1] + substitutionCost, // substitution
+			);
+		}
+	}
 
-    return matrix[an][bn];
+	return matrix[an][bn];
 }
